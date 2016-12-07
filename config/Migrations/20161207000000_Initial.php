fd7dfe2edd956cf61f3a972a0eb59060261cfea5
Tamási Benjamin <halftome@yahoo.com>
2016-12-07 21:29:32 +0100

    Initial migration

---
+++ config/Migrations/20161207000000_Initial.php
@@ -0,0 +1,36 @@
+<?php
+
+use Migrations\AbstractMigration;
+
+/**
+ * Created by PhpStorm.
+ * User: halftome
+ * Date: 2016. 12. 07.
+ * Time: 19:33
+ */
+class Initial extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('scheduler_jobs');
+        $table
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('running', 'boolean', [
+                'default' => false,
+                'null' => false,
+            ])
+            ->addColumn('last_run', 'datetime', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('last_run_finished', 'datetime', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->create();
+    }
+}
